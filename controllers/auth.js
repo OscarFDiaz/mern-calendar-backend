c90ce5ed15e1178a8c2c0ee921424e474b91275c
Oscar <chikenlitle99@hotmail.com>
2023-06-09 21:47:35 -0600

    Bcrypt añadido

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,39 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
+const crearUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password,
-  });
+  try {
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      res.status(400).json({
+        ok: false,
+        msg: 'Un usuario existe con ese correo',
+      });
+    }
+
+    usuario = new Usuario(req.body);
+
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador',
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
---
+++ models/Usuario.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    requiere: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    requiere: true,
+  },
+});
+
+module.exports = model('Usuario', UsuarioSchema);
