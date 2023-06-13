3293e67ba12c95c6ff6e30633adbccfa5729c96e
Oscar <chikenlitle99@hotmail.com>
2023-06-12 19:37:26 -0600

    CORS agregado

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 
 const { dbConnection } = require('./database/config');
 
+const cors = require('cors');
+
 //* Crear el servidor de express
 const app = express();
 
 //* Base de datos
 dbConnection();
+
+//* CORS
+app.use(cors());
 
 //* Directorio público
 app.use(express.static('public'));
